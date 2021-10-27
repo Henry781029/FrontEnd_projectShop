b3b5c1e6396aca077555bb024aae6ccec1face5d
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-27 12:05:52 -0500

    se crea ruta login

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 import React from 'react'
-import { Button } from '@mui/material'
-import { makeStyles } from '@mui/styles'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login/Login'
 
-const useStyles = makeStyles({
-    root:{
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        border: 0,
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        color: 'white',
-        height: 48,
-        padding: '0 30px',
-    },
-});
-
-export default function App () {
-    const classes = useStyles()
+export const App = () => {
     return (
-        <div>
-            <Button
-            variant ="contained"
-            className={classes.root}
-            >MATERIAL UI</Button>
-        </div>
+       <Router>
+            <Switch>
+               <Route
+               exact pacth = "/"><Login/>
+               </Route>
+           </Switch>
+       </Router>
     )
 }
 
 
+export default App;
