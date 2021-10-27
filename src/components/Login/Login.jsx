a2167d8ce3aaa136a9b0773c356eb7e38dd3b429
Henry781029 <61300603+Henry781029@users.noreply.github.com>
2021-10-27 14:38:16 -0500

    se crean rutas de back

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -113,7 +113,7 @@ export default function Login() {
                   </Link>
                 </Grid>
                 <Grid item>
-                  <Link href="#" variant="body2">
+                  <Link href="http://localhost:3000/register" variant="body2">
                     {"Don't have an account? Sign Up"}
                   </Link>
                 </Grid>
